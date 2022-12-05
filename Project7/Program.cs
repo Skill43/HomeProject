57c443fbe5de3aaaf4ff823ec9bbbac37f7a75f9
Илья <ilacikoridze357@gmail.com>
2022-12-05 19:52:15 +0300

    неделя

---
+++ Project7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите чиcло от 1 до 7: ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+if (d ==1){System.Console.WriteLine("Понедельник");}
+else if (d ==2){System.Console.WriteLine("Вторник");}
+else if (d ==3){ System.Console.WriteLine("Среда");}
+else if (d ==4){System.Console.WriteLine("Четверг");}
+else if (d ==5){System.Console.WriteLine("Пятница");}
+else if (d ==6){System.Console.WriteLine("Суббота");}
+else if (d ==7){System.Console.WriteLine("Воскресенье");}
+else System.Console.WriteLine("Ошибка!!!");
