87795a6b8ced9a5b3342e8705677742485ebbf1e
Илья <ilacikoridze357@gmail.com>
2022-12-05 15:23:48 +0300

    Макс и мин трех чисел

---
+++ Project2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int result = b > a ? b :a;
+result = result > c ? result : c;
+System.Console.WriteLine("Максимальное число: "+""+ result);
+int res = b < a ? b :a;
+res = res < c ? res : c;
+System.Console.WriteLine("Минимальное число: "+""+ res);
