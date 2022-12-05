ffb96d9d11415c859bc9ff36ea9d02f5b7f0c2ad
Илья <ilacikoridze357@gmail.com>
2022-12-05 15:15:08 +0300

    Макс из двух чисел

---
+++ Project1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите чиcло: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе чиcло: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int i= a>b ? a : b;
+Console.WriteLine($"Число {i} максимальное!");
