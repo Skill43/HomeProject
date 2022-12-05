1c454c02f28e82e2534d31ae26c1608a7ad0b2ba
Илья <ilacikoridze357@gmail.com>
2022-12-05 15:26:40 +0300

    Четное нечетное

---
+++ Project3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+string result = (num % 2 == 0) ? $"Число {num} четное!" : $"Число {num} нечетное!";
+System.Console.WriteLine(result);
